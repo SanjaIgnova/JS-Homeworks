2614fdea3c5716526cb3f5b8a339fb1bd6953a1a
SanjaIgnova <118814193+SanjaIgnova@users.noreply.github.com>
2022-12-24 08:56:57 +0100

    Add files via upload

---
+++ Homework #1-function/script.js
@@ -0,0 +1,26 @@
+function studentExercise(input){
+    let exercies = input
+    return exercies
+    
+
+}
+
+// object
+// studentExercise()
+// console.log(studentExercise());
+
+// booleans
+studentExercise(true);
+console.log(studentExercise(true));
+
+// numbers
+studentExercise(155);
+console.log(studentExercise(155));
+
+// strings
+studentExercise('Hello Sanja');
+console.log(studentExercise('Hello Sanja'));
+
+// undefined
+studentExercise(undefined);
+console.log(studentExercise(undefined));
